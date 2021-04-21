822822895df66da5b3a3cd652debbb65dde58707
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-21 15:43:44 +0200

    Add menu to services tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import logo from '../../assets/logo.svg';
 
@@ -52,17 +54,47 @@ const useStyles = makeStyles(theme => ({
     marginRight: '25px',
     height: '45px',
   },
+  menu: {
+    backgroundColor: theme.palette.common.primaryColor,
+    color: 'white',
+    borderRadius: 0,
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    '&:hover': {
+      opacity: 1,
+    },
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   useEffect(() => {
     const path = window.location.pathname;
 
     if (path === '/' && value !== 0) setValue(0);
-    if (path === '/services' && value !== 1) setValue(1);
+    if (path === '/services' && value !== 1) {
+      setValue(1);
+      setSelectedIndex(0);
+    }
+    if (path === '/customsoftware' && value !== 1) {
+      setValue(1);
+      setSelectedIndex(1);
+    }
+    if (path === '/mobileapps' && value !== 1) {
+      setValue(1);
+      setSelectedIndex(2);
+    }
+    if (path === '/websites' && value !== 1) {
+      setValue(1);
+      setSelectedIndex(3);
+    }
     if (path === '/revolution' && value !== 2) setValue(2);
     if (path === '/about' && value !== 3) setValue(3);
     if (path === '/contact' && value !== 4) setValue(4);
@@ -72,6 +104,29 @@ const Header = () => {
   const handleChange = (e, value) => {
     setValue(value);
   };
+
+  const handleClick = e => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(i);
+  };
+
+  const handleClose = e => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const menuOptions = [
+    { name: 'Services', link: '/services' },
+    { name: 'Custome Software Development', link: '/customsoftware' },
+    { name: 'Mobile App Development', link: '/mobileapps' },
+    { name: 'Website Development', link: '/websites' },
+  ];
 
   return (
     <>
@@ -106,7 +161,10 @@ const Header = () => {
                 label="Home"
               />
               <Tab
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup={anchorEl ? true : undefined}
                 className={classes.tab}
+                onMouseOver={e => handleClick(e)}
                 component={Link}
                 to="/services"
                 label="Services"
@@ -141,6 +199,32 @@ const Header = () => {
               >
                 Free Estimate
               </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                classes={{ paper: classes.menu }}
+                elevation={0}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{ onMouseLeave: handleClose }}
+              >
+                {menuOptions.map((option, i) => (
+                  <MenuItem
+                    key={option}
+                    component={Link}
+                    to={option.link}
+                    classes={{ root: classes.menuItem }}
+                    onClick={e => {
+                      handleMenuItemClick(e, i);
+                      setValue(1);
+                      handleClose(e);
+                    }}
+                    selected={i === selectedIndex && value === 1}
+                  >
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </Menu>
             </Tabs>
           </Toolbar>
         </AppBar>
