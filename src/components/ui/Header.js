a1bce47fb2f70fddf76aa46d05fc0013fa244e75
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-21 16:57:18 +0200

    Add media queries for the header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -9,6 +9,8 @@ import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 import logo from '../../assets/logo.svg';
 
@@ -29,9 +31,21 @@ const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: '3em',
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '2em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '1.25em',
+    },
   },
   logo: {
     height: '8em',
+    [theme.breakpoints.down('md')]: {
+      height: '7em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5.5em',
+    },
   },
   logoContainer: {
     padding: 0,
@@ -70,6 +84,8 @@ const useStyles = makeStyles(theme => ({
 
 const Header = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
@@ -128,6 +144,84 @@ const Header = () => {
     { name: 'Website Development', link: '/websites' },
   ];
 
+  const tabs = (
+    <>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={classes.tabContainer}
+        indicatorColor="primary"
+      >
+        <Tab className={classes.tab} component={Link} to="/" label="Home" />
+        <Tab
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup={anchorEl ? true : undefined}
+          className={classes.tab}
+          onMouseOver={e => handleClick(e)}
+          component={Link}
+          to="/services"
+          label="Services"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/revolution"
+          label="The Revolution"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/about"
+          label="About Us"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/contact"
+          label="Contact Us"
+        />
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/estimate"
+          onClick={() => {
+            setValue(5);
+          }}
+        >
+          Free Estimate
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          classes={{ paper: classes.menu }}
+          elevation={0}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{ onMouseLeave: handleClose }}
+        >
+          {menuOptions.map((option, i) => (
+            <MenuItem
+              key={option}
+              component={Link}
+              to={option.link}
+              classes={{ root: classes.menuItem }}
+              onClick={e => {
+                handleMenuItemClick(e, i);
+                setValue(1);
+                handleClose(e);
+              }}
+              selected={i === selectedIndex && value === 1}
+            >
+              {option.name}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Tabs>
+    </>
+  );
+
   return (
     <>
       <ElevationScroll>
@@ -148,84 +242,7 @@ const Header = () => {
                 src={logo}
               />
             </Button>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              className={classes.tabContainer}
-              indicatorColor="primary"
-            >
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/"
-                label="Home"
-              />
-              <Tab
-                aria-owns={anchorEl ? 'simple-menu' : undefined}
-                aria-haspopup={anchorEl ? true : undefined}
-                className={classes.tab}
-                onMouseOver={e => handleClick(e)}
-                component={Link}
-                to="/services"
-                label="Services"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/revolution"
-                label="The Revolution"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/about"
-                label="About Us"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/contact"
-                label="Contact Us"
-              />
-              <Button
-                className={classes.button}
-                variant="contained"
-                color="secondary"
-                component={Link}
-                to="/estimate"
-                onClick={() => {
-                  setValue(5);
-                }}
-              >
-                Free Estimate
-              </Button>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                classes={{ paper: classes.menu }}
-                elevation={0}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{ onMouseLeave: handleClose }}
-              >
-                {menuOptions.map((option, i) => (
-                  <MenuItem
-                    key={option}
-                    component={Link}
-                    to={option.link}
-                    classes={{ root: classes.menuItem }}
-                    onClick={e => {
-                      handleMenuItemClick(e, i);
-                      setValue(1);
-                      handleClose(e);
-                    }}
-                    selected={i === selectedIndex && value === 1}
-                  >
-                    {option.name}
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Tabs>
+            {matches ? null : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
